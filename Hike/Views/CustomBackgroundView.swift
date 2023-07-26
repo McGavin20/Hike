50537facf63c8c6407948a145b85b61252014cd1
McGavin20 <86229307+McGavin20@users.noreply.github.com>
2023-07-26 20:21:49 +0200

    MARK

--- Hike/Views/CustomBackgroundView.swift
+++ Hike/Views/CustomBackgroundView.swift
@@ -10,16 +10,16 @@ import SwiftUI
 struct CustomBackgroundView: View {
     var body: some View {
         ZStack {
-            //: DEPTH
+            //MARK -: DEPTH
             Color.customGreenDark
                 .cornerRadius(40)
                 .offset(y: 12)
-            //: LIGHT
+            //MARK -: LIGHT
             Color.customGreenLight
                 .cornerRadius(40)
                 .offset(y: 3)
                 .opacity(0.85)
-            //: SURFACE
+            //MARK -: SURFACE
             
             LinearGradient(colors: [.customGreenLight, .customGreenMedium], startPoint: .top, endPoint: .bottom)
                 .cornerRadius(40)
