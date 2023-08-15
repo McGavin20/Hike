96f19600433db1d4d16109bd920cd5408933a8e7
Sharma <smuridzo20@gmail.com>
2023-08-15 11:16:29 +0200

    Gradient Button Style

---
+++ Hike/View-Models/Gradient.swift
@@ -0,0 +1,22 @@
+//
+//  Gradient.swift
+//  Hike
+//
+//  Created by Sharma on 15/08/2023.
+//
+
+import Foundation
+import SwiftUI
+
+struct GradientButton: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration
+            .label
+            .padding(.vertical)
+            .padding(.horizontal, 30)
+            .background(
+                LinearGradient(colors: [.customGrayLight, .customGrayMedium], startPoint: .top, endPoint: .bottom)
+            )
+            .cornerRadius(40)
+    }
+}
