e0fe1dba2565d9d1dd100674fec3b94a51159193
Sharma <smuridzo20@gmail.com>
2023-08-23 17:59:38 +0200

    Final App

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct SettingsView: View {
+    
+    private let alternateAppsIcons: [String] = [
+        "AppIcon-MagnifyingGlass",
+        "AppIcon-Map",
+        "AppIcon-Mushroom",
+        "AppIcon-Camera",
+        "AppIcon-Backpack",
+        "AppIcon-Campfire"
+    ]
+    
     var body: some View {
-        
-        private let alternateAppIcons: [String] = [
-            "AppIcon-MagnifyingGlass",
-            "AppIcon-Map",
-            "AppIcon-Mushroom",
-            "AppIcon-Camera",
-            "AppIcon-Backpack",
-            "AppIcon-Campfire"
-        ]
         List {
             //HEADER
             Section {
@@ -59,7 +60,40 @@ struct SettingsView: View {
             }//SECTION
             .listRowSeparator(.hidden)
             //ICONS
-            
+            Section(header: Text("Alternate Icons")) {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 12) {
+                        ForEach(alternateAppsIcons.indices, id: \.self) { item in
+                            Button {
+                                print("Icon \(alternateAppsIcons[item]) was pressed")
+                                UIApplication.shared.setAlternateIconName(alternateAppsIcons[item]) { error in
+                                    if error != nil {
+                                        print("Failed request to update the app's icon: \(String(describing: error?.localizedDescription))")
+                                    } else {
+                                        print("Success! You have changed the app's icon to \(alternateAppsIcons[item])")
+                                    }
+                                }
+                                
+                            } label: {
+                                Image("\(alternateAppsIcons[item])-Preview")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80, height: 80)
+                                    .cornerRadius(16)
+                            }
+                        .buttonStyle(.borderless)
+                        }
+                    }
+                }
+                .padding(.top, 12)
+                Text("Choose your favourite app icon from the collective above.")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    .padding(.bottom, 12)
+            }
+            .listRowSeparator(.hidden)
             
             //ABOUT
             Section(
